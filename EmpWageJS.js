9740e93fbbbfea649efec8034d7f41f6d10f9a97
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-19 02:34:47 +0530

    Adding UC6

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -15,15 +15,22 @@ function getWorkingHours(empCheck) {
     }
 }  
  
-//UC 5 Calculating Wages till Number of Working Days or total working hrs per month is Reached
-const MAX_HRS_IN_MONTH = 100;
-const NUM_OF_WORKING_DAYS = 10;
+//UC 6 Storing Daily Wage in Array
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calcDailyWage(totalEmpHrs);
 console.log("Total Days : " + totalWorkingDays + "  Total Hours : " + totalEmpHrs + "  Employee Wage :" + empWage);
