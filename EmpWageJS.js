af5a06d937b5f26b4fd8b423489f52338742758b
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-19 01:52:06 +0530

    Adding UC3

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -1,31 +1,22 @@
-//UC 1 Check Employee Pesence 
-const IS_ABSENT = 0
-
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
-   } else {
-    console.log("Employee is Present");
-}
-
-// UC 2 Ability to Calculate Daily Eamplyee Wage 
+// UC 3 Refactor code to write a function to get working hrs
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}  
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME: 
-         empHrs = PART_TIME_HOURS;
-         break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-         break;
-    default:
-        empHrs = 0;
-}
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage: " + empWage)
+console.log("Emplopyee Wage" + empWage);
