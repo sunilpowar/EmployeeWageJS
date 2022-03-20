f28498f4d79076e092b045a5ec1bbe3d3987a0c9
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-20 15:39:34 +0530

    Adding UC10

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -23,6 +23,8 @@ const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+
+
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();    //UC 8
 let empDailyHrsMap = new Map();     //UC9
@@ -129,3 +131,27 @@ console.log("Full Working Days : " + fullWorkingDays);
 console.log("Part Working Days : " + partWorkingDays);
 console.log("Non Working Days : " + nonWorkingDays);
 
+// UC 10 Object Creation
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push(
+        {
+            dayNum:totalWorkingDays,
+            dailyHours:empHrs,
+            dailyWage: calcDailyWage(empHrs),
+            toString() {
+                return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours + 
+                        'And Wage Earned = ' + this.dailyWage
+            },
+        }
+    );
+}
+ 
+console.log("UC 10 Showing Daily Hours Worked and Wage Earned : " + empDailyHrsAndWageArr);
