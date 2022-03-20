0955a9d6e13fec7917fd9306ac4f93f7d375750a
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-20 15:02:14 +0530

    Adding UC9

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -25,14 +25,16 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();    //UC 8
-
+let empDailyHrsMap = new Map();     //UC9
+ 
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
-    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));   //UC8
+    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));      //UC8
+    empDailyHrsMap.set(totalWorkingDays, empHrs);                     //UC9
 }
 let empWage = calcDailyWage(totalEmpHrs);
 console.log("Total Days : " + totalWorkingDays + "  Total Hours : " + totalEmpHrs + "  Employee Wage :" + empWage);
@@ -104,3 +106,26 @@ function totalWages(totalWage, dailyWage) {
     return totalWage + dailyWage;
 }
 console.log("UC 7A - Employee Wage Map totalHrs: " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+// UC 9 Arrow Functions
+
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+
+let totalHours = Array.from(empDailyHrsMap.values()).filter(dailyHours => dailyHours > 0).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+
+console.log("(9A- Employyee Wage With Arrow. : " + "   Total Hours : " + totalHours + " Total Wages : " + totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach( (value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days : " + fullWorkingDays);
+console.log("Part Working Days : " + partWorkingDays);
+console.log("Non Working Days : " + nonWorkingDays);
+
